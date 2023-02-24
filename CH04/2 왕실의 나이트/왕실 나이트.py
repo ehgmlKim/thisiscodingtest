8be77ce36e3d73743bac39e2d0fb5f34f5e2c9c2
ehgmlKim <dohee930@gmail.com>
2023-02-25 02:30:37 +0900

    115p / 2 / 왕실의 나이트 / ★☆☆

---
+++ CH04/2 왕실의 나이트/왕실 나이트.py
@@ -0,0 +1,21 @@
+now = input()
+row = 'abcdefgh'
+x = int(now[1])
+y = row.find(now[0])+1
+cnt1, cnt2 = 0, 0
+# 이동하는 방법 찾기
+# 1. 수평으로 2칸 이동한 뒤에 수직으로 1칸 이동하는 경우의 수
+if y>2:
+    cnt1 = 2
+else:
+    cnt1 = 1
+if x>1:
+    cnt1 *= 2
+# 2. 수직으로 2칸 이동한 뒤에 수평으로 1칸 이동하는 경우의 수
+if x>3:
+    cnt2 = 2
+else:
+    cnt2 =1
+if y>1:
+    cnt2 *= 2
+print(cnt1+cnt2)
