084a7ea8397ea8bac5df305552857e35e1f63d7b
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-25 11:30:00 +0900

    Update 왕실 나이트.py
    오른쪽 벽면에 위치해있을 때를 고려해주는 조건 추가

--- CH04/2 왕실의 나이트/왕실 나이트.py
+++ CH04/2 왕실의 나이트/왕실 나이트.py
@@ -5,17 +5,17 @@ y = row.find(now[0])+1
 cnt1, cnt2 = 0, 0
 # 이동하는 방법 찾기
 # 1. 수평으로 2칸 이동한 뒤에 수직으로 1칸 이동하는 경우의 수
-if y>2:
+if y>2 and y<7:
     cnt1 = 2
 else:
     cnt1 = 1
-if x>1:
+if x>1 and x<8:
     cnt1 *= 2
 # 2. 수직으로 2칸 이동한 뒤에 수평으로 1칸 이동하는 경우의 수
-if x>3:
+if x>3 and x<7:
     cnt2 = 2
 else:
     cnt2 =1
-if y>1:
+if y>1 and y<8:
     cnt2 *= 2
 print(cnt1+cnt2)
