bd3f0e6fe8f08a5d4909c8951aa605f6d540ce05
ehgmlKim <dohee930@gmail.com>
2023-03-11 12:41:20 +0900

    맞은 코드로 수정

--- CH05/4 미로 탈출/미로 탈출.py
+++ CH05/4 미로 탈출/미로 탈출.py
@@ -5,21 +5,17 @@ for _ in range(n):
     arr.append(list(map(int, input())))
 dd = [[-1, 0], [1, 0], [0, -1], [0, 1]] #상하좌우
 
-
-visited = [[0]*m for _ in range(n)]
-visited[0][0] = 1
-
-for x in range(n):
-    for y in range(m):
-        if arr[x][y] == 0:
-            pass
-        else:
-            for dx, dy in dd:
-                nx = x + dx
-                ny = y + dy
-                if 0 <= nx < n and 0 <= ny < m:  # 범위 안일 때
-                    if not visited[nx][ny] and arr[nx][ny] == 1:
-                        arr[nx][ny] = arr[x][y] + 1
-                        visited[nx][ny] = 1
+def bfs(x, y, arr):
+    for dx, dy in dd:
+        nx = x + dx
+        ny = y + dy
+        if x==n-1 and y==m-1:
+            return
+        if 0 <= nx < n and 0 <= ny < m:  # 범위 안일 때
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = arr[x][y] + 1
+                bfs(nx, ny,arr)
+bfs(0,0,arr)
 for a in arr:
     print(a)
+print(arr[n-1][m-1])
