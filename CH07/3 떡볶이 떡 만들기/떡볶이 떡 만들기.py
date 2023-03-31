f2a13fc132c59f6f613a868d3c1f92cd37290b4f
ehgmlKim <dohee930@gmail.com>
2023-04-01 01:39:21 +0900

    201p / 3 / 떡볶이 떡 만들기 / ★★☆

---
+++ CH07/3 떡볶이 떡 만들기/떡볶이 떡 만들기.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+
+def binary(start, end, m):
+    mid = (start + end) // 2
+    h = [t - mid for t in arr if t > mid]
+    if (start > end):
+        return end
+    if sum(h) >= m:
+        return binary(mid + 1, end, m)
+    else:
+        return binary(start, mid - 1, m)
+
+
+print(binary(1, arr[-1], m))
